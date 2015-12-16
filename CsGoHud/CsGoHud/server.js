94536e63ef14c0afe734b5fab418ae76785aa366
PeppeS <pspfolio@gmail.com>
2015-12-16 17:17:27 +0200

    Getting bomb planted info from data and console logging it

--- CsGoHud/CsGoHud/server.js
+++ CsGoHud/CsGoHud/server.js
@@ -1,6 +1,35 @@
 ﻿var http = require('http');
+var fs = require('fs');
 var port = process.env.port || 1337;
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+	var bombPlanted = false;
+	if (req.method === 'POST') {
+		console.log("handling post request...");
+		res.writeHead(200, { 'Content-Type' : 'text/html' });
+		
+		var body = '';
+		req.on('data', function (data) {
+			body += data;
+		});
+		
+		req.on('end', function () {
+			var test = JSON.parse(body);
+			if (test.hasOwnProperty('round') && test.round.hasOwnProperty('bomb')) { 
+				bombPlanted = true;
+			} else {
+				bombPlanted = false;
+			}
+			console.log("Bomb planted: " + bombPlanted)
+
+			res.end('<html><body>' + body + '</body></html>');
+		});
+	} else {
+		console.log("other type of req method");
+		res.writeHead(200, { 'Content-Type' : 'text/html' });
+		var html = '<html><body>Other type data</body></html>';
+		res.end(html);
+	}
+	res.end('Hello World\n');
 }).listen(port);
+console.log("Listening port: " + port);
